f3e5869605ece91157b43526579a454506a08872
humbertofarrapo <105745727+humbertofarrapo@users.noreply.github.com>
2023-01-10 10:15:41 -0300

    Update funcoescomplementares.h

--- funcoescomplementares.h
+++ funcoescomplementares.h
@@ -4,10 +4,10 @@ Cabecalhos das funcoes implementadas no modulo funcoescomplementares.c,
 que realizam pequenas validacoes e pequenas acoes na interface.
 **********************************************************************/
 
-int validarcpf(char cp_f[]);
+int validarcpf(char cp_f[]); //Valida um CPF (string) recebido em 0 ou 1;
 
-int validarsenha(char senha[]);
+int validarsenha(char senha[]); //Valida uma senha (string) recebida em 0 ou 1;
 
-void telalogin();
+void telalogin(); //Exibe a interface de login;
 
-void opArvBin();
+void opArvBin(); //Exibe a interface de menu de opcoes.
